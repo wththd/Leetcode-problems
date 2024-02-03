95f64bafbec7d463cd8bda79dafe807a4549988e
wththd <kolyagonza@gmail.com>
2024-02-03 11:23:21 +0300

    NumberPalindrome

---
+++ LeetCodeProblems/Easy/NumberPalindrome/NumberPalindrome.cs
@@ -0,0 +1,41 @@
+﻿using LeetCodeProblems.Basic;
+
+namespace LeetCodeProblems.Easy.NumberPalindrome;
+
+public class NumberPalindrome : BaseTest<int, bool>
+{
+    public override void Setup()
+    {
+        _inputs = new List<int>()
+        {
+            121,
+            -121,
+            10
+        };
+
+        _outputs = new List<bool>
+        {
+            true,
+            false,
+            false
+        };
+    }
+
+    protected override bool Solve(int input)
+    {
+        if (input < 0)
+        {
+            return false;
+        }
+
+        var reversedNumber = 0;
+        var targetNumber = input;
+        while (targetNumber != 0)
+        {
+            reversedNumber = reversedNumber * 10 + targetNumber % 10;
+            targetNumber /= 10;
+        }
+
+        return reversedNumber == input;
+    }
+}
