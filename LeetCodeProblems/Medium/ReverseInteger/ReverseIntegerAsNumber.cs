98f48dc072ff06eea4a4e39d6705408911f954ef
wththd <kolyagonza@gmail.com>
2024-02-03 11:12:16 +0300

    Reverse integer

---
+++ LeetCodeProblems/Medium/ReverseInteger/ReverseIntegerAsNumber.cs
@@ -0,0 +1,43 @@
+﻿using LeetCodeProblems.Basic;
+
+namespace LeetCodeProblems.Medium.ReverseInteger;
+
+public class ReverseIntegerAsNumber : BaseTest<int, int>
+{
+    public override void Setup()
+    {
+        _inputs = new List<int>
+        {
+            123,
+            -123,
+            120,
+            -2147483412
+        };
+
+        _outputs = new List<int>
+        {
+            321,
+            -321,
+            21,
+            -2143847412
+        };
+    }
+
+    protected override int Solve(int input)
+    {
+        var result = 0;
+        
+        while (input != 0)
+        {
+            var addition = input % 10;
+            input /= 10;
+            if (int.MaxValue / 10 < result || int.MinValue / 10 > result)
+            {
+                return 0;
+            }
+            result = result * 10 + addition;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCodeProblems/Medium/ReverseInteger/ReverseIntegerAsString.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using LeetCodeProblems.Basic;
+
+namespace LeetCodeProblems.Medium.ReverseInteger;
+
+public class ReverseIntegerAsString : BaseTest<int, int>
+{
+    public override void Setup()
+    {
+        _inputs = new List<int>
+        {
+            123,
+            -123,
+            120,
+            -2147483412
+        };
+
+        _outputs = new List<int>
+        {
+            321,
+            -321,
+            21,
+            -2143847412
+        };
+    }
+
+    protected override int Solve(int input)
+    {
+        var result = new StringBuilder();
+        var inputAsString = input.ToString();
+        for (var i = inputAsString.Length - 1; i >= 0 ; i--)
+        {
+            var value = inputAsString[i];
+            if (value == '-')
+            {
+                result.Insert(0, value);
+            }
+            else
+            {
+                result.Append(value);
+            }
+        }
+
+        if (int.TryParse(result.ToString(), out var number))
+        {
+            return number;
+        }
+
+        return 0;
+    }
+}
